84615b201ea76abaef0a89595607662352d47073
Nikotine <nikita12360732@gmail.com>
2023-11-01 20:22:47 +0300

    map is done, search is next

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import MedItem from './UI/MedItem';
 
 export default function MainPage({ medicines }) {
   return (
     <main role="main">
       <ul className="entries-list no-bullets no-padding">
-        {medicines?.map((med) => (
-          <MedItem key={med.id} med={med} />
-        ))}
+        <Row>
+          {medicines?.map((med) => (
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <MedItem key={med.id} med={med} />
+            </Col>
+          ))}
+        </Row>
       </ul>
     </main>
   );
